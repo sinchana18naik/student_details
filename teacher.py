20c2eaa513bc92020b54d31ba8659f10699e325a
goliboy <mudholhound866@gmail.com>
2025-10-30 15:57:51 +0530

    save it

--- teacher.py
+++ teacher.py
@@ -1,6 +1,7 @@
 name=str(input("enter teacher name"))
 sub=str(input("enter teacher subject"))
 code=str(input("enter subject code"))
+print ("teacher details")
 
 print(f"name is:{name}")
 print(f"subject:{sub}")
